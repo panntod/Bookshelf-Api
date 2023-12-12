aaeb00acbb17ce2f86e17e8c570570e9d99b187d
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-12-12 18:22:53 +0700

    feat: added function update book

--- src/handler.js
+++ src/handler.js
@@ -15,13 +15,15 @@ exports.addBookHandler = (request, h) => {
         const response = h.response({
             status: 'fail',
             message: 'Gagal menambahkan buku, isi nama terlebih dahulu'
-        })
+        }).code(400)
+        return response;
     }
     else if(readPage > pageCount){
         const response = h.response({
             status: 'fail',
             message: "Gagal menambahkan buku, readPage tidak boleh lebih besar dari pageCount"
-        })
+        }).code(400)
+        return response;
     }
 
     book.push(newBook)
@@ -34,16 +36,14 @@ exports.addBookHandler = (request, h) => {
             data: {
                 bookId: id,
             }
-        })
-            .code(201);
+        }).code(201);
         return response;
     }
 
     const response = h.response({
         status: 'fail',
         message: 'Buku gagal di tambaahkan',
-    })
-    response.code(500);
+    }).code(500);
     return response;
 }
 
@@ -98,4 +98,48 @@ exports.getDetailBookByIdHandler = (request, h) => {
         message: "Buku tidak ditemukan"
     }).code(404)
     return response
+}
+
+exports.editItemByIdHandler = (request, h) => {
+    const bookId = request.params
+    const { name, year, author, summary, publisher, pageCount, readPage, reading, } = request.payload
+
+    if(!name){
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal menambahkan buku, isi nama terlebih dahulu'
+        }).code(400)
+        return response;
+    }
+    else if(readPage > pageCount){
+        const response = h.response({
+            status: 'fail',
+            message: "Gagal menambahkan buku, readPage tidak boleh lebih besar dari pageCount"
+        }).code(400)
+        return response;
+    }
+
+    const finished = pageCount === readPage
+    const updatedAt = new DataTransfer().toString()
+
+    const index = book.findIndex((books) => books.id === bookId)
+
+    if(index !== -1){
+        book[index] = {
+            ...items[index], name, year, author, summary, publisher, pageCount, readPage, reading, finished, updatedAt,
+        };
+
+        const response = h.response({
+            status: 'success',
+            message: 'Buku berhasil diperbarui',
+        }).code(200);
+        return response;
+    }
+
+    const response = h
+    .response({
+        status: 'fail',
+        message: 'Gagal memperbarui buku. Id tidak ditemukan',
+    }).code(404);
+    return response;
 }
